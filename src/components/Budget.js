50e37a7ceea38bfe43ddf5502ad172e1a0f83c6c
Daniel Gomez <dgomez@linq.com>
2024-05-09 11:10:45 -0500

    Format files

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,42 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+
+const BUDGET_UPPER_LIMIT = 20000;
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-        </div>
+  const { budget, expenses, dispatch } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+
+  const handleBudgetChange = (event) => {
+    const totalExpenses = expenses.reduce(
+      (total, item) => (total += item.cost),
+      0
     );
+    const nBudget = event.target.value;
+    if (nBudget > BUDGET_UPPER_LIMIT) {
+      alert("The budget cannot exceed £" + BUDGET_UPPER_LIMIT);
+      return;
+    } else if (nBudget < totalExpenses) {
+      alert("The budget cannot be lower than £" + totalExpenses);
+      return;
+    } else {
+      setNewBudget(nBudget);
+      dispatch({
+        type: "SET_BUDGET",
+        payload: nBudget,
+      });
+    }
+  };
+  return (
+    <div className='alert alert-secondary'>
+      <span>Budget: £ </span>
+      <input
+        type='number'
+        step='10'
+        value={newBudget}
+        onChange={handleBudgetChange}
+      ></input>
+    </div>
+  );
 };
 export default Budget;
